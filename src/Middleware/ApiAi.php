5f6e93a31bc43998510fc3de2eb332cb789338f2
Nikita <luckyraul@users.noreply.github.com>
2018-01-09 20:04:55 +0300

    fix constuct parameter

--- src/Middleware/ApiAi.php
+++ src/Middleware/ApiAi.php
@@ -8,173 +8,168 @@ use BotMan\BotMan\Interfaces\HttpInterface;
 use BotMan\BotMan\Interfaces\MiddlewareInterface;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 
-class ApiAi implements MiddlewareInterface {
+class ApiAi implements MiddlewareInterface
+{
     /** @var string */
     protected $token;
-    
+
     /** @var HttpInterface */
     protected $http;
-    
+
     /** @var stdClass */
     protected $response;
-    
+
     /** @var string */
     protected $lastResponseHash;
-    
+
     /** @var string */
     protected $apiUrl = 'https://api.api.ai/v1/query?v=20150910';
-    
+
     /** @var bool */
-    protected $listenForAction = FALSE;
-    
+    protected $listenForAction = false;
+
     /**
-     * API.ai constructor.
-     *
-     * @param string        $token api.ai access token
+     * Wit constructor.
+     * @param string $token api.ai access token
+     * @param string $lang language
      * @param HttpInterface $http
      */
-    public function __construct($token, $lang = 'en', HttpInterface $http) {
+    public function __construct($token, $lang = 'en', HttpInterface $http)
+    {
         $this->token = $token;
         $this->lang  = $lang;
         $this->http  = $http;
     }
-    
+
     /**
-     * Create a new API.ai middleware instance.
-     *
+     * Create a new Wit middleware instance.
      * @param string $token api.ai access token
-     * @param string $lang  language
-     *
+     * @param string $lang language
      * @return ApiAi
      */
-    public static function create($token, $lang = 'en') {
-        return new static($token, new Curl());
+    public static function create($token, $lang = 'en')
+    {
+        return new static($token, $lang, new Curl());
     }
-    
+
     /**
      * Restrict the middleware to only listen for API.ai actions.
-     *
-     * @param bool $listen
-     *
+     * @param  bool $listen
      * @return $this
      */
-    public function listenForAction($listen = TRUE) {
+    public function listenForAction($listen = true)
+    {
         $this->listenForAction = $listen;
-        
+
         return $this;
     }
-    
+
+    /**
+     * Perform the API.ai API call and cache it for the message.
+     * @param  \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
+     * @return stdClass
+     */
+    protected function getResponse(IncomingMessage $message)
+    {
+        $response = $this->http->post($this->apiUrl, [], [
+            'query' => [$message->getText()],
+            'sessionId' => md5($message->getConversationIdentifier()),
+            'lang' => $this->lang,
+        ], [
+            'Authorization: Bearer '.$this->token,
+            'Content-Type: application/json; charset=utf-8',
+        ], true);
+
+        $this->response = json_decode($response->getContent());
+
+        return $this->response;
+    }
+
     /**
      * Handle a captured message.
      *
      * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
-     * @param BotMan                                           $bot
-     * @param                                                  $next
+     * @param BotMan $bot
+     * @param $next
      *
      * @return mixed
      */
-    public function captured(IncomingMessage $message, $next, BotMan $bot) {
+    public function captured(IncomingMessage $message, $next, BotMan $bot)
+    {
         return $next($message);
     }
-    
+
     /**
      * Handle an incoming message.
      *
      * @param IncomingMessage $message
-     * @param BotMan          $bot
-     * @param                 $next
+     * @param BotMan $bot
+     * @param $next
      *
      * @return mixed
      */
-    public function received(IncomingMessage $message, $next, BotMan $bot) {
+    public function received(IncomingMessage $message, $next, BotMan $bot)
+    {
         $response = $this->getResponse($message);
-        
-        $reply            = isset($response->result->fulfillment->speech) ? $response->result->fulfillment->speech : '';
-        $action           = isset($response->result->action) ? $response->result->action : '';
-        $actionIncomplete = isset($response->result->actionIncomplete) ? (bool) $response->result->actionIncomplete : FALSE;
-        $intent           = isset($response->result->metadata->intentName) ? $response->result->metadata->intentName : '';
-        $parameters       = isset($response->result->parameters) ? (array) $response->result->parameters : [];
-        
+
+        $reply = isset($response->result->fulfillment->speech) ? $response->result->fulfillment->speech : '';
+        $action = isset($response->result->action) ? $response->result->action : '';
+        $actionIncomplete = isset($response->result->actionIncomplete) ? (bool) $response->result->actionIncomplete : false;
+        $intent = isset($response->result->metadata->intentName) ? $response->result->metadata->intentName : '';
+        $parameters = isset($response->result->parameters) ? (array) $response->result->parameters : [];
+
         $message->addExtras('apiReply', $reply);
         $message->addExtras('apiAction', $action);
         $message->addExtras('apiActionIncomplete', $actionIncomplete);
         $message->addExtras('apiIntent', $intent);
         $message->addExtras('apiParameters', $parameters);
-        
+
         return $next($message);
     }
-    
-    /**
-     * Perform the API.ai API call and cache it for the message.
-     *
-     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
-     *
-     * @return stdClass
-     */
-    protected function getResponse(IncomingMessage $message) {
-        $response = $this->http->post($this->apiUrl, [], [
-            'query'     => [$message->getText()],
-            'sessionId' => md5($message->getConversationIdentifier()),
-            'lang'      => $this->lang,
-        ], [
-                                          'Authorization: Bearer ' . $this->token,
-                                          'Content-Type: application/json; charset=utf-8',
-                                      ], TRUE);
-        
-        $this->response = json_decode($response->getContent());
-        
-        return $this->response;
-    }
-    
+
     /**
      * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
-     * @param string                                           $pattern
-     * @param bool                                             $regexMatched Indicator
-     *                                                                       if
-     *                                                                       the
-     *                                                                       regular
-     *                                                                       expression
-     *                                                                       was
-     *                                                                       matched
-     *                                                                       too
-     *
+     * @param string $pattern
+     * @param bool $regexMatched Indicator if the regular expression was matched too
      * @return bool
      */
-    public function matching(IncomingMessage $message, $pattern, $regexMatched) {
+    public function matching(IncomingMessage $message, $pattern, $regexMatched)
+    {
         if ($this->listenForAction) {
-            $pattern = '/^' . $pattern . '$/i';
-            
-            return (bool) preg_match($pattern,
-                                     $message->getExtras()[ 'apiAction' ]);
+            $pattern = '/^'.$pattern.'$/i';
+
+            return (bool) preg_match($pattern, $message->getExtras()['apiAction']);
         }
-        
-        return TRUE;
+
+        return true;
     }
-    
+
     /**
      * Handle a message that was successfully heard, but not processed yet.
      *
      * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
-     * @param BotMan                                           $bot
-     * @param                                                  $next
+     * @param BotMan $bot
+     * @param $next
      *
      * @return mixed
      */
-    public function heard(IncomingMessage $message, $next, BotMan $bot) {
+    public function heard(IncomingMessage $message, $next, BotMan $bot)
+    {
         return $next($message);
     }
-    
+
     /**
      * Handle an outgoing message payload before/after it
      * hits the message service.
      *
-     * @param mixed  $payload
+     * @param mixed $payload
      * @param BotMan $bot
-     * @param        $next
+     * @param $next
      *
      * @return mixed
      */
-    public function sending($payload, $next, BotMan $bot) {
+    public function sending($payload, $next, BotMan $bot)
+    {
         return $next($payload);
     }
 }
